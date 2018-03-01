ffbc7cc911dbbbb3b9b64d982f60a2e72f306743
xav <xavierartot@gmail.com>
2018-02-28 22:08:36 -0600

    search news dynamically

--- src/News/News.js
+++ src/News/News.js
@@ -8,25 +8,28 @@ export default class News extends React.Component {
       news:  [],
     }
   }
-  componentDidMount() {
-    const url = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=a48e707253534c0c91f20d599bfd33a2';
-    fetch(url)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        this.setState({
-          news: data.articles
-        });
-      })
-    .catch((err) => console.log(err));
-  }
   renderItems = () => {
-    return this.state.news.map( (item) => {
-      return <NewSingle key={item.url} item={item} />;
-    });
+    if (this.state.news) {
+      return this.state.news.map( (item) => <NewSingle key={item.url} item={item} />);
+    }
   };
   render() {
+    if (this.props.search) {
+      console.log(1);
+      const url = `https://newsapi.org/v2/everything?q=${this.props.search}&apiKey=a48e707253534c0c91f20d599bfd33a2`;
+      console.log(url);
+      console.log(this.props.search);
+      fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.setState({
+            news: data.articles
+          });
+        })
+      .catch((err) => console.log(err));
+    }
     return (
       <div className="row">
      {this.renderItems()}
--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,40 @@ import './App.css';
 import News from '../News/News';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '' 
+    };
+  }
+  searchForm = (event) => {
+    event.preventDefault()
+    this.setState({
+      search: this.search.value
+    });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">My feed</h1>
+          <form className='col s12' onSubmit={e => this.searchForm(e)}>
+            <div className="row">
+              <div className="input-field col s12">
+                <input 
+                  ref={(input) => { this.search = input; }}
+                  defaultValue=""
+                  placeholder="...Search" 
+                  type="text" 
+                  className="validate" 
+                />
+                <button className="btn btn-primary" type="submit">send</button>
+              </div>
+
+            </div>
+          </form>
         </header>
-        <News />
+        <News search={this.state.search} />
       </div>
     );
   }
